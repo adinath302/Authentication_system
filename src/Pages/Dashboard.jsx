543ef3ffb3107dfd3d78bd9f3b7648c05886e871
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-11-30 12:27:09 +0530

    Delete src directory

--- src/Pages/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Welcome = () => {
-  return (
-    <h1 className='items-center text-2xl flex justify-center font-semibold'>Welcome</h1>
-  )
-}
-
-export default Welcome
